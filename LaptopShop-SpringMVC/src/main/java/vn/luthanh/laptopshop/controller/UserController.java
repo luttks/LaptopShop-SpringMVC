97a6b0a1a524ada83caa690f2469a5eb8511ca2c
Lu Thanh <luthanh102@gmail.com>
2025-04-03 12:07:26 +0700

    update 3/4

--- LaptopShop-SpringMVC/src/main/java/vn/luthanh/laptopshop/controller/UserController.java
+++ LaptopShop-SpringMVC/src/main/java/vn/luthanh/laptopshop/controller/UserController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -36,6 +37,13 @@ public class UserController {
         System.out.println("Check users: " + users);
         model.addAttribute("users1", users);
         return "admin/user/table-user";
+    }
+
+    @RequestMapping("/admin/user/{id}")
+    public String getUserDetailPage(Model model, @PathVariable Long id) {
+        System.err.println("Check id: " + id);
+        model.addAttribute("id", +id);
+        return "admin/user/show";
     }
 
     @RequestMapping("/admin/user/create")
--- LaptopShop-SpringMVC/src/main/webapp/WEB-INF/view/admin/user/table-user.jsp
+++ LaptopShop-SpringMVC/src/main/webapp/WEB-INF/view/admin/user/table-user.jsp
@@ -43,11 +43,12 @@
                     <tbody>
                         <c:forEach var="user" items="${users1}">
                             <tr>
+                                
                                 <th>${user.id}</th>
                                 <td>${user.email}</td>
                                 <td>${user.fullName}</td>
                                 <td>
-                                    <button class="btn btn-success">View</button>
+                                    <a href="/admin/user/${user.id}" class="btn btn-success">View</a>
                                     <button class="btn btn-warning mx-2">Update</button>
                                     <button class="btn btn-danger">Delete</button>
                                 </td>
